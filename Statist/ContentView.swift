df488cef22786275ab5447658484801f72b16bf6
era_ser <eraser3031@gmail.com>
2021-07-04 22:57:51 +0900

    Initial Commit

---
+++ Statist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Statist
+//
+//  Created by Kimyaehoon on 04/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Statist/StatistApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatistApp.swift
+//  Statist
+//
+//  Created by Kimyaehoon on 04/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct StatistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
