b81e3295a154d2ae7e60e8d939b632ec693dad6e
era_ser <eraser3031@gmail.com>
2021-07-26 20:05:50 +0900

    feat: Add StatViewModel

--- Statist/Core/Stat/ViewModels/StatViewModel.swift
+++ Statist/Core/Stat/ViewModels/StatViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import CoreData
+
+class StatViewModel: ObservableObject {
+    @Published var todoEntitys: [TodoListEntity] = []
+    var percent: Int {
+        let value: Double = todoEntitys.reduce(0) { result, entity in
+            if entity.isDone {
+                return result + 1
+            } else {
+                return result
+            }
+        }
+        let percent = value / Double(todoEntitys.count) * 100
+        return Int(percent)
+    }
+    
+    @Published var timetableEntityGroups: [[TimetableEntity]] = []
+    
+    @Published var progressEntitys: [ProgressEntity] = []
+    
+    let manager = CoreDataManager.instance
+    
+    init() {
+        getTodoListEntitys(start: Date().toDay(), end: Date().prevWeek().toDay())
+        getTimetableEntityGroups()
+        getProgressEntitys()
+    }
+    
+    private func getTodoListEntitys(start: Date, end: Date) {
+        let request = NSFetchRequest<TodoListEntity>(entityName: "TodoListEntity")
+        let filter = NSPredicate(format: "date >= %@ AND date <= %@", start as NSDate, end as NSDate)
+        request.predicate = filter
+        
+        do {
+            todoEntitys = try manager.context.fetch(request)
+        } catch let error {
+            print("Error Fetching TodoListEntitys(by StatViewModel) \(error)")
+        }
+    }
+    
+    
+    
+    private func getTimetableEntityGroups() {
+        
+    }
+    
+    private func getProgressEntitys() {
+        
+        let request = NSFetchRequest<ProgressEntity>(entityName: "ProgressEntity")
+
+        do {
+            progressEntitys = try manager.context.fetch(request)
+        } catch let error {
+            print("Error Fetching ProgressEntitys(by StatViewModel \(error)")
+        }
+    }
+}
