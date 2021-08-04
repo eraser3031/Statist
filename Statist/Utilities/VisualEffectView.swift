77a5b28f5a9c41531d54ad76de55bee558dcbcd1
era_ser <eraser3031@gmail.com>
2021-08-04 23:14:04 +0900

    feat: add visulaeffectview

---
+++ Statist/Utilities/VisualEffectView.swift
@@ -0,0 +1,19 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+Abstract:
+The iOS implementation of a UIVisualEffectView's blur and vibrancy.
+*/
+
+import SwiftUI
+
+struct VisualEffectView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
